b7a8dc291196b35bfcc3607374fd85383271cba3
Aditya Kumar <2020akumar@tjhsst.edu>
2019-07-12 09:17:23 -0400

    Added end user file

---
+++ endUser.py
@@ -0,0 +1,49 @@
+import pickle
+import v_processMicrophone
+import c_1
+import e_peaksToDict
+from collections import counter
+import numpy as np
+pickleName="database.pickle"
+print('Loading Song Database')
+with open(pickleName,"rb") as file:
+    database=pickle.load(file)
+print("Database Loaded")
+userWantsContinue=True
+def matchRecordToSong(recordedFingerprints):
+    '''Take fingerprints from recorded sample and find matches in songs
+
+    Paramaters:
+    -----------------------------------
+    recordedFingerprints: Any iterable with the recorded fingerprints from the 10 sec sample
+    -----------------------------------
+    Returns:
+        Most popular song found or "No song found" '''
+    arr=np.array(recordedFingerprints)
+    counts=Counter(arr[arr in database])
+    print(counts)
+    dict={key:value for (key,value) in counts}
+    orderedMatches=sorted(A, key=A.get)
+    orderedValues=list(dict.matches())
+    return orderedMatches[0]
+
+
+
+
+while (userWantsContinue):
+    digSamples=v_processMicrophone.processAudio(10)
+    peaks=c_1.Samples_to_Peaks(digSamples)
+    fingerprints=peaks_to_Dict(peaks)
+    matchedSongInfo=matchRecordToSong(fingerprints)
+    print(matchedSongInfo)
+    answer="None"
+    while answer.lower() not in ["Yes","No"]:
+        answer=input("Type yes if you want to enter another recording or no if you're done")
+        answer=answer.lower()
+        if answer =="yes":
+            userWantsContinue=True
+        elif answer=="no":
+            userWantsContinue=False
+        else:
+            answer="None"
+
