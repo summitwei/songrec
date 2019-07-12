620d4d4d9bb729471eb822288e20737adae7680a
Charles Richards <richardsc@bxscience.edu>
2019-07-12 10:43:43 -0400

    Second Prototype

--- C_1.py
+++ C_1.py
@@ -1,24 +1,31 @@
 import C_RyanFUNCS as f
 import numpy as np
 import matplotlib.mlab as mlab
+
+
 def Samples_to_Peaks(samples):
     '''Takes in a 2D array of numpy music sample and returns peaks
     Input()
     _______________
     SAMPLE
 
-    
+
     Output()
     _______________
     (frequency , time)'''
+    hello=0 #Just testing git
+    x = samples
+    NumofSamp = lambda l: 44100 * len(l)
+    sampling_rate = 44100
+    Time = np.linspace(0, len(x) / 44100,
+                       len(x))  # This starts at 0, ends at what should be the time, len(x), break by frequency
+    frequency = np.arange(len(samples) // 2 + 1 / NumofSamp(samples))
+    # print (frequency)
+    s = np.abs(np.fft.rfft(samples))
+    Spectrogram, freqs, t = mlab.specgram(s, NFFT=4096, Fs=sampling_rate,
+                                          window=mlab.window_hanning,
+                                          noverlap=int(4096 / 2))  # samples to Spectrogram
+    peaks = f.local_peaks(np.log(Spectrogram), .77, 20)
 
-
-    NumofSamp = lambda l:44100*len(l)
-    Time = np.linspace(0,len(x),44100*len(x)) #This starts at 0, ends at what should be the time, len(x), break by frequency
-    frequency = np.arange(len(samples[x])//2+1/NumofSamp(samples[x]))
-    #print (frequency)
-    Spectrogram, freqs, t = mlab.specgram(np.abs(np.fft.rfft(samples[x])), Time )#samples to Spectrogram
-    peaks =f.local_peaks(np.log(Spectrogram), .77, 20)
-    
-    return(peaks)
-        #print(Spectrogram)
+    return (peaks)
+    # print(Spectrogram)
