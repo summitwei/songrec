744d1a6d3170b33ba2e3615e92913397c951e5c5
Charles Richards <richardsc@bxscience.edu>
2019-07-11 23:52:31 -0400

    Not done yet

--- C_1.py
+++ C_1.py
@@ -1,17 +1,20 @@
-import C_Ryan'sFUNCS
-import Numpy as np
+import C_RyanFUNCS
+import numpy as np
 def Samples_to_Peaks(samples):
-'''Takes in an array of numpy music sample and returns peaks
-	Input()
-	_______________
-	SAMPLE
-	
-	Output()
-	_______________
-	(frequency , time)'''
-	NumofSamp = lambda l:44100*len(l)
-	frequency = np.arange(len(sample)//2+1/NumofSamp(sample))
-	print (frequency)
-	Spectrogram = (samples)#samples to Spectrogram
-	
+    '''Takes in a 2D array of numpy music sample and returns peaks
+    Input()
+    _______________
+    SAMPLE
+
+    
+    Output()
+    _______________
+    (frequency , time)'''
+    for x in range(len(samples)):
+        NumofSamp = lambda l:44100*len(l)
+        Time = np.arange(len(x)/)
+        frequency = np.arange(len(samples[x])//2+1/NumofSamp(samples[x]))
+        #print (frequency)
+        Spectrogram = (np.log(np.abs(np.fft.rfft(samples[x]))), )#samples to Spectrogram
+        print(Spectrogram)
 Samples_to_Peaks([[0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6]])
